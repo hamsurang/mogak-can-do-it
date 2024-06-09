acc99a9b0b2cb1df2190e144fd19ee8c43ddde4d
Chung-il Jung <jungchungil7@gmail.com>
2024-06-09 15:59:41 +0900

    feat: panda css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <></>;
-};
+import { css } from '../styled-system/css';
+
+function App() {
+  return (
+    <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>
+      Hello 🐼!
+    </div>
+  );
+}
 
 export default App;
