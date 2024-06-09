80b1fc98b023e30ac4d8532b8b6c6d3d853896ab
Chung-il Jung <jungchungil7@gmail.com>
2024-06-09 16:38:18 +0900

    radix, react router dom, error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,32 @@
+import '@radix-ui/themes/styles.css';
 import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Theme } from '@radix-ui/themes';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import ErrorPage from './pages/Error.tsx';
+import MogakPage from './pages/Mogak.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'about',
+    element: <MogakPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <RouterProvider router={router} />
+    </Theme>
   </React.StrictMode>,
 );
---
+++ src/pages/Mogak.tsx
@@ -0,0 +1,9 @@
+const MogakPage = () => {
+  return (
+    <div>
+      <h1>모각코</h1>
+    </div>
+  );
+};
+
+export default MogakPage;
