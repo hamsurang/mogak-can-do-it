c4e65fa8bc3b99713995f1f99e4bef12cb9452f8
minsour <sparkm0128@gmail.com>
2024-06-09 16:39:56 +0900

    fix: fix lint error

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import '@radix-ui/themes/styles.css';
 import './index.css';
 
+import { Theme } from '@radix-ui/themes';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { Theme } from '@radix-ui/themes';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import ErrorPage from './pages/Error.tsx';
 import MogakPage from './pages/Mogak.tsx';
